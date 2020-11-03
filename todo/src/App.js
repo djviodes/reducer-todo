3282a0936bcad70bacd3c6497a6cb344223708a9
djviodes <djviodes26@gmail.com>
2020-11-02 16:05:52 -0800

    I will come back to finish

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,22 +1,43 @@
+import React, { useReducer } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import reducer from './reducers/reducers';
+import { setItem, setCompleted, setId } from './actions/actions';
 
 function App() {
+  const initialState = {
+    item: '',
+    completed: false,
+    id: 0
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleChanges = event => {
+    dispatch(setItem(event.target.value));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <input  
+          id='todo'
+          type='text'
+          name='todo'
+          value={state.item}
+          onChange={handleChanges}
+        />
+        <button
+          onClick={() => {
+            dispatch(setItem(state.item));
+            dispatch(setCompleted(state.completed));
+            dispatch(setId(state.id));
+          }}
         >
-          Learn React
-        </a>
+          Submit
+        </button>
       </header>
     </div>
   );
---
+++ todo/src/reducers/reducers.js
@@ -0,0 +1,16 @@
+import { SET_ITEM, SET_COMPLETED, SET_ID } from '../actions/actions';
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case(SET_ITEM):
+            return({...state, item: action.payload});
+        case(SET_COMPLETED):
+            return({...state, completed: action.payload});
+        case(SET_ID):
+            return({...state, id: action.payload});
+        default:
+            return(state);
+    }
+}
+
+export default reducer;
